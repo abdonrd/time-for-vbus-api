9117ababd67d44449119855b76353c6abecbdf9a
Abdón Rodríguez Davila <a@abdonrd.com>
2015-05-07 20:28:14 +0200

    Init the Vitrasa service

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # coding=utf-8
 
-from flask import Flask
+from flask import Flask, g
+
+from vitrasa import Vitrasa
 
 
 app = Flask(__name__)
+
+
+@app.before_request
+def init_vitrasa():
+    g.vitrasa = Vitrasa()
 
 
 if __name__ == '__main__':
