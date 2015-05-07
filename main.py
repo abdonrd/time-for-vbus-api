7a77c26d7f8d9721e7da74110e337525d6cce476
Abdón Rodríguez Davila <a@abdonrd.com>
2015-05-07 20:33:29 +0200

    Create the principal error handlers

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 # coding=utf-8
 
-from flask import Flask, g
+from flask import Flask, g, jsonify
 
 from vitrasa import Vitrasa
 
 
 app = Flask(__name__)
+
+
+def create_error_response(error):
+    response = {
+        'error': {
+            'code': error.code,
+            'message': error.name
+        }
+    }
+
+    return jsonify(response), error.code
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return create_error_response(error)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return create_error_response(error)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return create_error_response(error)
 
 
 @app.before_request
